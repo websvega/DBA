c973cfb89248b61648b360c85dde9a8721241ff6
mvaldesb <mau_v_b20@hotmail.com>
2016-09-14 14:51:55 -0500

    hola

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -33,21 +33,20 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(254, 109);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(190, 89);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(199, 121);
+            this.button1.Size = new System.Drawing.Size(149, 98);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(717, 321);
+            this.ClientSize = new System.Drawing.Size(538, 261);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
